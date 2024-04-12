64f904e38f5b1e7c0306b9b77affe2b2776eb605
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-04-12 19:02:17 +0800

    update, change mystiko rust to rev c009564 (#24)
    * update, chage mystiko rust to rev c009564
    
    * update, fix lint error

--- monitor/tests/monitor_tests.rs
+++ monitor/tests/monitor_tests.rs
@@ -12,8 +12,9 @@ use mystiko_protos::data::v1::Commitment;
 use mystiko_protos::sequencer::v1::sequencer_service_server::SequencerServiceServer;
 use mystiko_protos::sequencer::v1::{
     ChainLoadedBlockRequest, ChainLoadedBlockResponse, ContractLoadedBlockRequest, ContractLoadedBlockResponse,
-    FetchChainRequest, FetchChainResponse, GetCommitmentsRequest, GetCommitmentsResponse, GetNullifiersRequest,
-    GetNullifiersResponse, HealthCheckRequest, HealthCheckResponse,
+    FetchChainRequest, FetchChainResponse, GetCommitmentsByTxHashRequest, GetCommitmentsByTxHashResponse,
+    GetCommitmentsRequest, GetCommitmentsResponse, GetNullifiersByTxHashRequest, GetNullifiersByTxHashResponse,
+    GetNullifiersRequest, GetNullifiersResponse, HealthCheckRequest, HealthCheckResponse,
 };
 use mystiko_protos::service::v1::ServerOptions;
 use mystiko_roller_monitor::{start_monitor, start_monitor_with_config, RollerMonitor, RollerMonitorConfig};
@@ -265,7 +266,8 @@ mock! {
       async fn get_commitments(&self, _request: tonic::Request<GetCommitmentsRequest>) -> Result<Response<GetCommitmentsResponse>, Status>;
       async fn get_nullifiers(&self, _request: tonic::Request<GetNullifiersRequest>) -> Result<Response<GetNullifiersResponse>, Status>;
       async fn health_check(&self, _request: Request<HealthCheckRequest>,) -> Result<Response<HealthCheckResponse>,Status>;
-
+      async fn get_commitments_by_tx_hash(&self, request: Request<GetCommitmentsByTxHashRequest>) -> Result<Response<GetCommitmentsByTxHashResponse>, Status>;
+      async fn get_nullifiers_by_tx_hash(&self, request: Request<GetNullifiersByTxHashRequest>) -> Result<Response<GetNullifiersByTxHashResponse>, Status>;
   }
 }
 
